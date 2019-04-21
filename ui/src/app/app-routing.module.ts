c8e76989e09af0f2dd52c8844415b59f245203be
alex.k <alex.k>
2019-04-21 20:30:28 +0300

    bootstrap added, quizzess component initial commit

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {QuizzesComponent} from "./components/quizzes/quizzes.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: QuizzesComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
